c63e21a5c20d5ba7f413af9b9886fb1f86b6927c
ROP Kipchumba Allan <ropallank@gmail.com>
2022-09-28 09:36:01 +0300

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string
- *
- * @s: string
+ * _put recursion - prints a string
+ * @s:pointer block of memmory to fill
+ * Return:void
  */
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
 		_putchar('\n');
+		return;
 	}
-	else
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
+	_putchar(*s);
+
+	_puts_recursion(s + 1);
 }
